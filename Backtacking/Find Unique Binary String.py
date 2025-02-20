eb89b5a80295a240676e2cc67e49859a2ee05714
Kalki767 <kalkidankidane3@gmail.com>
2025-02-20 22:19:28 +0300

    Find Unique Binary String

---
+++ Backtacking/Find Unique Binary String.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        nums = set(nums)
+        def backtrack(index):
+            if index == len(nums):
+                if "".join(result) not in nums:
+                    return "".join(result)
+                
+                return ""
+            non_pick = backtrack(index+1)
+            if non_pick:
+                return non_pick
+            result[index] = '1'
+            pick = backtrack(index+1)
+            result[index] = '0'
+            if pick:
+                return pick
+            
+            return ""
+        result = ['0' for _ in range(len(nums))]
+        if "".join(result) not in nums:
+            return "".join(result)
+        for i in range(len(nums)-1,-1,-1):
+            result[i] = '1'
+            answer = backtrack(i+1)
+            if answer:
+                return answer
+        
